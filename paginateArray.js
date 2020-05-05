be54679da90f1af913ec5ca673d53bc4e6cb355d
LukaszGW <lukasz.girzycki@gmail.com>
2020-05-05 20:15:00 +0200

    poprawiona funkcja paginateArray

--- paginateArray.js
+++ paginateArray.js
@@ -5,19 +5,63 @@ let dataEntries = [31, 30, 304, 303, 304, 305, 307, 307, 306, 307, 307, 307,
    306, 37, 306, 306, 307, 36, 307, 306, 307, 307, 300, 307, 307, 307, 307,
    306, 306, 307, 306, 306, 307, 307, 306, 305, 307, 307, 307, 307, 307, 29,
    22, 286, 257, 27, 257, 257, 257, 257, 257, 257, 257, 257, 25, 257, 257,
-   257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257]
+   257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 200, 300,
+   31, 30, 304, 303, 304, 305, 307, 307, 306, 307, 307, 307,
+   307, 307, 307, 307, 299, 304, 306, 307, 306, 304, 307, 306,307, 306, 307,
+   307, 307, 305, 305, 306, 307, 307, 305, 307, 306, 307, 307, 307, 307, 36,
+   306, 37, 306, 306, 307, 36, 307, 306, 307, 307, 300, 307, 307, 307, 307,
+   306, 306, 307, 306, 306, 307, 307, 306, 305, 307, 307, 307, 307, 307, 29,
+   22, 286, 257, 27, 257, 257, 257, 257, 257, 257, 257, 257, 25, 257, 257,
+   257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 200, 300,
+   31, 30, 304, 303, 304, 305, 307, 307, 306, 307, 307, 307,
+   307, 307, 307, 307, 299, 304, 306, 307, 306, 304, 307, 306,307, 306, 307,
+   307, 307, 305, 305, 306, 307, 307, 305, 307, 306, 307, 307, 307, 307, 36,
+   306, 37, 306, 306, 307, 36, 307, 306, 307, 307, 300, 307, 307, 307, 307,
+   306, 306, 307, 306, 306, 307, 307, 306, 305, 307, 307, 307, 307, 307, 29,
+   22, 286, 257, 27, 257, 257, 257, 257, 257, 257, 257, 257, 25, 257, 257,
+   257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 200, 300,
+   31, 30, 304, 303, 304, 305, 307, 307, 306, 307, 307, 307,
+   307, 307, 307, 307, 299, 304, 306, 307, 306, 304, 307, 306,307, 306, 307,
+   307, 307, 305, 305, 306, 307, 307, 305, 307, 306, 307, 307, 307, 307, 36,
+   306, 37, 306, 306, 307, 36, 307, 306, 307, 307, 300, 307, 307, 307, 307,
+   306, 306, 307, 306, 306, 307, 307, 306, 305, 307, 307, 307, 307, 307, 29,
+   22, 286, 257, 27, 257, 257, 257, 257, 257, 257, 257, 257, 25, 257, 257,
+   257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 200, 300,
+   31, 30, 304, 303, 304, 305, 307, 307, 306, 307, 307, 307,
+   307, 307, 307, 307, 299, 304, 306, 307, 306, 304, 307, 306,307, 306, 307,
+   307, 307, 305, 305, 306, 307, 307, 305, 307, 306, 307, 307, 307, 307, 36,
+   306, 37, 306, 306, 307, 36, 307, 306, 307, 307, 300, 307, 307, 307, 307,
+   306, 306, 307, 306, 306, 307, 307, 306, 305, 307, 307, 307, 307, 307, 29,
+   22, 286, 257, 27, 257, 257, 257, 257, 257, 257, 257, 257, 25, 257, 257,
+   257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 200, 300,
+   31, 30, 304, 303, 304, 305, 307, 307, 306, 307, 307, 307,
+   307, 307, 307, 307, 299, 304, 306, 307, 306, 304, 307, 306,307, 306, 307,
+   307, 307, 305, 305, 306, 307, 307, 305, 307, 306, 307, 307, 307, 307, 36,
+   306, 37, 306, 306, 307, 36, 307, 306, 307, 307, 300, 307, 307, 307, 307,
+   306, 306, 307, 306, 306, 307, 307, 306, 305, 307, 307, 307, 307, 307, 29,
+   22, 286, 257, 27, 257, 257, 257, 257, 257, 257, 257, 257, 25, 257, 257,
+   257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 257, 200, 300]
 
-/*settings zmienna z ustawieniami dotyczącymi indeksu strony (actualPageIdx) i
-*/
+//settings zmienna z ustawieniami dotyczącymi indeksu strony (actualPageIdx)
 let settings = {actualPageIdx:9, entriesOnPage:50};
 
-// Stwórz funkcję paginateArray. Funkcja zwraca ilość elementów na podstawie ustawionego indexu i ogranicza do 50
+
+// Stwórz funkcję paginateArray
 const paginateArray = (dataEntries, settings) => {
-    let entriesOnSelectedPage = dataEntries[settings.actualPageIdx];
-    console.log("Obecna liczba elementów na stronie ${entriesOnSelectedPage}");
-    if (entriesOnSelectedPage > settings.entriesOnPage) {
-      console.log("obecna liczba elementów ${entriesOnSelectedPage}. Ograniczam liczbę elementów do ${settings.entriesOnPage}")
-      entriesOnSelectedPage = settings.entriesOnPage
+  entriesToArray = []
+  for (let i=0; i < dataEntries.length; i += settings.entriesOnPage) {
+      entriesToArray.push(dataEntries.slice(i, i + settings.entriesOnPage));
+  }
+  /*Walidacja dataEntries czy jest Arrayem, i czy zawiera jakieś elementy do paginacji.
+  Warunek waliduje również czy*/
+  if (settings != null && dataEntries != null) {
+    if (Array.isArray(dataEntries) && Boolean(dataEntries[0]) && typeof(settings.actualPageIdx) == "number" && typeof(settings.entriesOnPage) == "number") && entriesToArray[actualPageIdx] != undefinded
+    {
+      let entriesOnSelectedPage = entriesToArray[settings.actualPageIdx];
+      return entriesOnSelectedPage;
+      };
     }
-    return entriesOnSelectedPage;
-};
+  else {
+    console.log("Błędnie wprowadzone dane. Sprawdź i wprowadź je jeszcze raz")
+  }
+}
